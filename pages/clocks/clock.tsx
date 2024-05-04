0c1a3b6796f185b9d0ffb6ae2c2ca0ce5317582b
Aiden McIlraith <a.mcilraith@wustl.edu>
2024-05-03 19:55:04 -0700

    Working on clocks

--- pages/clocks/clock.tsx
+++ pages/clocks/clock.tsx
@@ -1,37 +1,93 @@
-import { Typography } from "@mui/material";
+import { Grid, Menu, MenuItem, TextField, Typography } from "@mui/material";
+import { useState } from "react";
 import { ResponsiveContainer, PieChart, Pie, Cell } from 'recharts';
 
+// eslint-disable-next-line @next/next/no-img-element
+const img = <img src="/clock_outline.png" alt="clock" style={
+    {
+        position: 'absolute',
+        width: '100%',
+        height: '100%',
+        top: 0,
+        left: 0,
+        pointerEvents: 'none',
+    }
+} />
 
-export type clockType = { size: number, filled: number, title: string }
-export const Clock = ({ clock: { size, filled, title }, setClock }: { clock: clockType, setClock: (arg: clockType) => void }) => {
+export type clockType = { size: number, filled: number, title: string, uuid: string }
+export const Clock = ({ clock: { size, filled, title, ...clock }, setClock }: { clock: clockType, setClock: (arg: clockType) => void }) => {
     const on = { value: 1, on: true };
     const off = { value: 1, on: false }
     const data01 = [...[...new Array(size - filled)].map(() => off), ...[...new Array(filled)].map(() => on)];
     const radius = 80;
     const radRatio = .86
-    return <div style={{ width: radius * 2 }}>
-        <div style={{ height: radius * 2, width: radius * 2, position: "relative" }}>
-            <ResponsiveContainer width="100%" height="100%" style={{ transform: "rotate(-90deg)" }}>
-                <PieChart width={100} height={100} >
-                    <Pie data={data01} dataKey="value" cx="50%" cy="50%" outerRadius={radRatio * radius}  >
+    const [contextMenu, setContextMenu] = useState<{
+        mouseX: number;
+        mouseY: number;
+    } | null>(null);
+    return <Grid item style={{ width: radius * 2 }} sx={{ margin: 1 }}>
+        <div style={{ height: radius * 2, width: radius * 2, position: "relative" }}
+            onContextMenu={event => {
+                event.preventDefault();
+                setContextMenu(
+                    contextMenu === null
+                        ? {
+                            mouseX: event.clientX + 2,
+                            mouseY: event.clientY - 6,
+                        }
+                        : // repeated contextmenu when it is already open closes it with Chrome 84 on Ubuntu
+                        // Other native context menus might behave different.
+                        // With this behavior we prevent contextmenu from the backdrop to re-locale existing context menus.
+                        null,
+                );
+            }}>
+            <ResponsiveContainer width="100%" height="100%" style={{ transform: "rotate(-90deg)" }} >
+                <PieChart width={100} height={100}
+
+                >
+                    <Pie data={data01} dataKey="value" cx="50%" cy="50%" outerRadius={radRatio * radius} isAnimationActive={false}  >
                         {data01.map((entry, index) => (
-                            <Cell key={`cell-${index}`} fill={entry.on ? "#d12317" : "#0000"} stroke={"#888"} onClick={() => setClock({ filled: entry.on ? size-index-1 : size-index, title, size })} />
+                            <Cell key={`cell-${index}`} fill={entry.on ? "#d12317" : "#0000"} stroke={"#888"} style={{ outline: 'none' }}
+                                onClick={(e) => {
+                                    e.preventDefault();
+                                    e.bubbles = false;
+                                    setClock({
+                                        filled: size - index === filled ? size - index - 1 : size - index,
+                                        title,
+                                        size,
+                                        ...clock
+                                    });
+                                }}
+                                onDrag={e => console.log(e.movementX)}
+
+                            />
                         ))}
                     </Pie>
                 </PieChart>
             </ResponsiveContainer>
-            <img src="/clock_outline.png" alt="clock" style={
-                {
-                    position: 'absolute',
-                    width: '100%',
-                    height: '100%',
-                    top: 0,
-                    left: 0,
-                    pointerEvents: 'none',
+            {img}
+            <Menu
+                open={contextMenu !== null}
+                onClose={() => setContextMenu(null)}
+                anchorReference="anchorPosition"
+                anchorPosition={
+                    contextMenu !== null
+                        ? { top: contextMenu.mouseY, left: contextMenu.mouseX }
+                        : undefined
                 }
-            } />
+            >
+                {[3, 4, 6, 8, 12].map(n => <MenuItem key={n}
+                    onClick={() => {
+                        setClock({ size: n, filled, title, ...clock });
+                        setContextMenu(null);
+                    }}
+                >{n}</MenuItem>)}
+            </Menu>
         </div>
-        <Typography width="100%" textAlign="center">{title}</Typography>
-    </div>
+        <TextField value={title} onChange={(v) => {
+            v.preventDefault();
+            setClock({ size, filled, title: v.target.value, ...clock })
+        }} />
+    </Grid>
 }
 
--- pages/clocks/index.tsx
+++ pages/clocks/index.tsx
@@ -1,17 +1,41 @@
 "use client";
 
 import Link from 'next/link'
-import { Button, List, ListItem } from "@mui/material";
+import { AppBar, Button, Container, List, ListItem, Paper } from "@mui/material";
 import { Clock, clockType } from './clock';
 import { useState } from 'react';
 // import { useLocalStorage } from '@uidotdev/usehooks';
 
+import * as React from 'react';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import Badge from '@mui/material/Badge';
+import Grid from '@mui/material/Grid';
+import MenuIcon from '@mui/icons-material/Menu';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import { theme } from '@/imports/theme';
+
+function makeid(length: number) {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    let counter = 0;
+    while (counter < length) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        counter += 1;
+    }
+    return result;
+}
+
 const Clocks = () => {
     // const [clocks, setClocks] = useLocalStorage<clockType[]>("clockData", [])
-    const [clocks, setClocks] = useState<clockType[]>( [])
+    const [clocks, setClocks] = useState<clockType[]>([])
     return (
-        <div>
-            CLOCKS!
+        <Container>
             <List>
                 <ListItem>
                     <Link href="/">Home</Link>
@@ -20,18 +44,91 @@ const Clocks = () => {
                     <Link href="/clocks">Clocks</Link>
                 </ListItem>
             </List>
-            {clocks.map(
-                (clock, index) => <Clock
-                    key={clock.title + clock.size + clock.filled}
-                    clock={clock}
-                    setClock={(newClock) => {
-                        clocks[index] = newClock;
-                        setClocks([...clocks])
-                    }}
-                />)}
-            <Button onClick={() => setClocks([...clocks, { filled: 0, size: 4, title: "New Clock" }])}>Add Clock</Button>
-        </div>
+            <Grid container>
+
+                {clocks.map(
+                    (clock, index) => <Clock
+                        key={clock.uuid}
+                        clock={clock}
+                        setClock={(newClock) => {
+                            clocks[index] = newClock;
+                            setClocks([...clocks])
+                        }}
+                    />)}
+            </Grid>
+            <Button onClick={() => setClocks([...clocks, { filled: 0, size: 4, title: "New Clock", uuid: makeid(12) }])}>Add Clock</Button>
+        </Container>
     );
 }
 
-export default Clocks;
+
+export default function Dashboard() {
+    const [open, setOpen] = React.useState(true);
+    const toggleDrawer = () => {
+        setOpen(!open);
+    };
+
+    return (
+        <ThemeProvider theme={theme}>
+            <Box sx={{ display: 'flex' }}>
+                <CssBaseline />
+                <AppBar position="absolute">
+                    <Toolbar
+                        sx={{
+                            pr: '24px', // keep right padding when drawer closed
+                        }}
+                    >
+                        <IconButton
+                            edge="start"
+                            color="inherit"
+                            aria-label="open drawer"
+                            onClick={toggleDrawer}
+                            sx={{
+                                marginRight: '36px',
+                                ...(open && { display: 'none' }),
+                            }}
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Typography
+                            component="h1"
+                            variant="h6"
+                            color="inherit"
+                            noWrap
+                            sx={{ flexGrow: 1 }}
+                        >
+                            Dashboard
+                        </Typography>
+                        <IconButton color="inherit">
+                            <Badge badgeContent={4} color="secondary">
+                                <NotificationsIcon />
+                            </Badge>
+                        </IconButton>
+                    </Toolbar>
+                </AppBar>
+                <Box
+                    component="main"
+                    sx={{
+                        backgroundColor: (theme) =>
+                            theme.palette.mode === 'light'
+                                ? theme.palette.grey[100]
+                                : theme.palette.grey[900],
+                        flexGrow: 1,
+                        height: '100vh',
+                        overflow: 'auto',
+                    }}
+                >
+                    <Toolbar />
+                    <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+                        <Paper>
+
+                            <Grid container spacing={3}>
+                                <Clocks />
+                            </Grid>
+                        </Paper>
+                    </Container>
+                </Box>
+            </Box>
+        </ThemeProvider>
+    );
+}
